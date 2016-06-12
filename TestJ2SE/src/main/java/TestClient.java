b4ec223fb3b4a7f82dde9309fca80c3246a5388b
lvhw <lvhw@fxiaoke.com>
2016-06-12 08:24:45 +0800

    test

---
+++ TestJ2SE/src/main/java/TestClient.java
@@ -0,0 +1,42 @@
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by lvhw on 2016/6/11.
+ */
+public class TestClient {
+    public static void main(String[] args) {
+        String name = null;
+        //System.out.println(name.equals("hongwei"));
+        System.out.println("hongwei".equals(name));
+
+        List<Integer> list = Arrays.asList(1, 2, 2, 4);
+
+        for (int i = 0, length=list.size(); i < length; i++) {
+            System.out.println(i);
+        }
+
+        try {
+            try{
+                throw new NullPointerException();
+
+            }catch (Exception e){
+                System.out.println("exception 1");
+                e.printStackTrace();
+            }
+
+            try{
+
+                throw new IOException();
+            }catch (Exception e){
+
+                System.out.println("exception 2");
+                e.printStackTrace();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ TestJ2SE/src/main/java/innerclass/EnclosingClass.java
@@ -0,0 +1,11 @@
+package innerclass;
+
+/**
+ * Created by lvhw on 2016/6/11.
+ */
+public class EnclosingClass {
+
+    public static class InnerClass {
+
+    }
+}
