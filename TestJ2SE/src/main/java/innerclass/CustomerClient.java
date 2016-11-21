d360b85715ffba17bc68ed179f9882f2c45b2ef6
lvhw <lvhw@fxiaoke.com>
2016-11-22 00:08:56 +0800

    del target folder

--- TestJ2SE/src/main/java/innerclass/CustomerClient.java
+++ TestJ2SE/src/main/java/innerclass/CustomerClient.java
@@ -7,6 +7,7 @@ import lambda.pojos.Person;
  * Created by lvhw on 2016/11/21 23:23.
  */
 public class CustomerClient {
+
     public static void main(String[] args) {
         BankAccount account = new BankAccount(1, 100000000, null);
         Person lvhw = new Person("lv", "hongwei", "engineer", "man", 28, 10000);
